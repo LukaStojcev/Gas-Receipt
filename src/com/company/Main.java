d9ecef22af06515e3152451a722924eb249be185
Luka <luka.stojcev.2023@student.lhschools.org>
2020-02-25 13:44:54 -0500

    Intellij Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("+-----------------------------------------------+");
+        System.out.println("|          Gas Station Gas Station              |");
+        System.out.println("|                                               |");
+        System.out.println("| 0000 Dead Man's Cove, Bermuda Tringle, 3.1415 |");
+        System.out.println("|                                               |");
+        System.out.println("| Date                   Time                   |");
+        System.out.println("| 30FEB2022              3:14 PM                |");
+        System.out.println("|                                               |");
+        System.out.println("| Invoice Number#        3141592                |");
+        System.out.println("| Auth#                                         |");
+        System.out.println("|                                               |");
+        System.out.println("|         Mastercard Account Number             |");
+        System.out.println("|              ************97                   |");
+        System.out.println("|                                               |");
+        System.out.println("|     Pump        Product       $/G             |");
+        System.out.println("|      3           UNLD        $2.30            |");
+        System.out.println("|                                               |");
+        System.out.println("|     Gallons      Fuel        Total            |");
+        System.out.println("|      3.14           $        7.23             |");
+        System.out.println("|                                               |");
+        System.out.println("|                 Thank You!                    |");
+        System.out.println("|          Gas Station Gas Station®             |");
+        System.out.println("+-----------------------------------------------+");
+    }
+}
